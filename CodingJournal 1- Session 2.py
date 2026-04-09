0385baf6a226d7bf1451988250ea0f88941c76d2
maqpeter-bit <maqpeter@ucsc.edu>
2026-04-08 18:06:06 -0700

    4-8-2026 (2)
    Added in Coding Journal 1's second assignment / session.

---
+++ CodingJournal 1- Session 2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:52:22 2026
+
+@author: mrdan
+"""
+
+a = 1.1
+b = 2.3
+
+c = 3
+d = 2
+
+print("Sum of two floating point numbers:")
+print(f"{a} + {b} = {a + b}")
+print(type(a + b), "\n")
+
+print("Difference between two integers:")
+print(f"{c} - {d} = {c - d}")
+print(type(c - d), "\n")
+
+print("Product of a floating point number and an integer:")
+print(f"{a} * {c} = {a * c}")
+print(type(a * c), "\n")
+
