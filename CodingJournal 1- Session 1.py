11056fec49f486289069cdcc3c735e8f218422f8
maqpeter-bit <maqpeter@ucsc.edu>
2026-04-08 17:46:53 -0700

    4-8-2026
    Added Coding Journal 1's session 1 assignment

---
+++ CodingJournal 1- Session 1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:30:35 2026
+
+@author: mrdan
+"""
+
+print("Mason Peters, Pronouns: He/Him, My favorite movie is Wes Anderson's Fantastic Mr. Fox, and my favorite food is spaghetti.")
