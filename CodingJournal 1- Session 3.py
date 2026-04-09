c61ece26a39aecae5faabcd7a223251f6142c9bb
maqpeter-bit <maqpeter@ucsc.edu>
2026-04-08 18:14:07 -0700

    4-8-2026
    Submitting Code Journal 1's assignment 3.

---
+++ CodingJournal 1- Session 3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 18:08:30 2026
+
+@author: mrdan
+"""
+
+def f(x):
+    return x**3 + 8
+
+def main():
+    a = f(9)
+    if (a > 27):
+        print("YAY!")
+    print(a)
+    
+if __name__ == "__main__":
+    main()
